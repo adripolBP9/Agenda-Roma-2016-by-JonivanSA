da2b5d180cffe9791f6c4868c6fd81ef0e85be2a
JonathanMorató <jmorato48@iesnumancia.cat>
2016-01-30 12:37:50 +0100

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -45,15 +45,12 @@ $JSView.controller = {
     	modal_info_aeroport: function(e){
 		$JSView.dataView({},e)	
 	}
-	
 	modal_info_coliseo: function(e){
 		$JSView.dataView({},e)	
 	}
-	
-	modal_info_sanprieto: function(e){
+	modal_info_sanpietro: function(e){
 		$JSView.dataView({},e)	
 	}
-	
 	modal_info_navona: function(e){
 		$JSView.dataView({},e)	
 	}
